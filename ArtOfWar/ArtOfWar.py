14a55a7d20a4491eec876df185f26e94ed4e2ea8
Joris <jorislit@gmail.com>
2020-08-04 19:51:42 +0200

    V1.01

--- ArtOfWar/ArtOfWar.py
+++ ArtOfWar/ArtOfWar.py
@@ -6,15 +6,13 @@ import subprocess
 
 
 DELAY_BETWEEN_COMMANDS = 2
-STARTUP_DELAY_PROGRAM = 20
+STARTUP_DELAY_PROGRAM = 5
 
 def main():
     # INIT
     pyautogui.FAILSAFE = True
-
-    os.startfile (r'C:\Users\Joris\Desktop\PyThon\Art of War.lnk')
-    #subprocess.Popen('Art of War')
-    #subprocess.Popen('C:\Program Files\BlueStacks\HD-RunApp.exe')
+    # Start Bluestack & Art of War
+    #os.startfile (r'C:\Users\Joris\Desktop\PyThon\ArtOfWar\Art of War.lnk')
 
     # countdown timer
     print("starting")
@@ -25,8 +23,15 @@ def main():
 
     #ReportMousePosition()
 
-    for seq in range(0, 10):
-        DoBattle()
+    #DoBattle(AmmountOfAtacks=5)
+
+    DoHeadHunt()
+    DoHeadHunt()
+    #DoHeadHunt()
+    #DoBattle()
+
+    #for seq in range(0, 10):
+    #    DoBattle()
 
     #DoHeadHunt()
     
@@ -34,8 +39,8 @@ def main():
     print("Done")
 
 
-def DoBattle():
-    for seq in range(1, 11):
+def DoBattle(AmmountOfAtacks=10):
+    for seq in range(1, AmmountOfAtacks + 1 ):
         #Select Battle
         pyautogui.click(480, 1422,duration=0.25)
         time.sleep(DELAY_BETWEEN_COMMANDS)
@@ -87,34 +92,36 @@ def DoHeadHunt():
 def DesertTreasureHunt():
      for seq in range(1, 11):
             #Select Easy mode
+            print("Start easy mode", seq )
             pyautogui.click(731, 601,duration=0.25)
             time.sleep(DELAY_BETWEEN_COMMANDS)
             #Start Attack
             pyautogui.click(473, 1426,duration=0.25)
             time.sleep(10 + random.randint(0, 5))
             #Watch video for extra gold
-            if seq > 6:
+            if seq > 4:
                 WatchVideo()
             else:
                 pyautogui.click(656, 1145,duration=0.25)
                 time.sleep(5+ random.randint(0, 5))
-            print("easy mode", seq)
+            print("Finish easy mode", seq)
 
 def WinterIsComing():
     for seq in range(1, 11):
             #Select Hard mode
+            print("Start Hard mode", seq )
             pyautogui.click(826, 1003,duration=0.25)
             time.sleep(DELAY_BETWEEN_COMMANDS)
             #Start Attack
             pyautogui.click(473, 1426,duration=0.25)
             time.sleep(15 + random.randint(0, 5))
             #Watch video for extra gold
-            if seq > 6:
+            if seq > 4:
                 WatchVideo()
             else:
                 pyautogui.click(656, 1145,duration=0.25)
                 time.sleep(5+ random.randint(0, 5))
-            print("Hard mode", seq)
+            print("Finish Hard mode", seq )
 
 def ReportMousePosition(seconds=10):
     for i in range(0, seconds):
